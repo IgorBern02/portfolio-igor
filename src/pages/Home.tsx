dbc19f4ec09cca2a0f2246a215b2da14961ec2ca
IgorBern02 <igor.bernardes2@gmail.com>
2026-09-15 18:40:08 -0300

    Refactor Home component to streamline section rendering and improve layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 import { Hero } from "../sections/Hero/Hero";
 import { About } from "../sections/About/About";
 import { Technologies } from "../sections/Technologies/Technologies";
+import { Projects } from "../sections/Projects/Projects";
+import { Contact } from "../sections/Contact/Contact";
 
 export function Home() {
   return (
-    <main className="w-full min-h-screen px-4 flex flex-col">
+    <main className="min-h-screen w-full">
       <Hero />
-      <section id="about">
-        <About />
-      </section>
-      <section id="technologies">
-        <Technologies />
-      </section>
+      <About />
+      <Technologies />
+      <Projects />
+      <Contact />
     </main>
   );
 }
