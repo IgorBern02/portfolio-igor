482f4decb3c5f8463dcdd536ef87de19b9da1108
IgorBern02 <igor.bernardes2@gmail.com>
2026-09-15 16:15:33 -0300

    Refactor Technologies section to use Card component for displaying technology cards

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -9,7 +9,6 @@ export function Card({ icon: Icon, children }: CardProps) {
   return (
     <div className="bg-card border border-border p-4 rounded-xl text-center">
       <Icon className="w-6 h-6 text-purple-400 mx-auto mb-2" />
-
       {children}
     </div>
   );
--- src/sections/Technologies/Technologies.tsx
+++ src/sections/Technologies/Technologies.tsx
@@ -1,14 +1,40 @@
-// import { Card } from "../../components/shared/Card";
+import { Card } from "../../components/shared/Card";
 import { TechnologiesBio } from "./TechnologiesBio";
 import { TechnologiesHighlights } from "./TechnologiesHighlights";
-// import {
-//   CiDatabase,
-//   FaLaptopCode,
-//   FaTools,
-//   FaFigma,
-// } from "../../components/ui/icons";
+import {
+  CiDatabase,
+  FaLaptopCode,
+  FaTools,
+  FaFigma,
+} from "../../components/ui/icons";
 
 export const Technologies: React.FC = () => {
+  const cards = [
+    {
+      icon: FaLaptopCode,
+      text: "Desenvolvimento Web",
+      description:
+        "Interfaces modernas, escaláveis e com foco na experiência do usuário.",
+    },
+    {
+      icon: CiDatabase,
+      text: "APIs e Backend",
+      description:
+        "Integrações e consumo de APIs, com Node.js, Express e bancos de dados.",
+    },
+    {
+      icon: FaTools,
+      text: "Versionamento",
+      description:
+        "Controle de versão com Git, colaboração e organização com Git e GitHub.",
+    },
+    {
+      icon: FaFigma,
+      text: "Design e Prototipação",
+      description:
+        "Criação de interfaces e protótipos com Figma, explorando design de interação e experiência do usuário.",
+    },
+  ];
   return (
     <section
       id="tecnologias"
@@ -22,40 +48,18 @@ export const Technologies: React.FC = () => {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center p-2">
-        <div className="lg:col-span-7">
+        <div className="lg:col-span-7 ">
           <TechnologiesBio />
-          {/* <Card icon={FaLaptopCode}>
-            <span className={"block text-xs text-text"}>
-              Desenvolvimento Web
-            </span>
-            <span className={"text-sm font-semibold text-muted-foreground"}>
-              Interfaces modernas, escaláveis e com foco na experiência do
-              usuário.
-            </span>
-          </Card>
-          <Card icon={CiDatabase}>
-            <span className={"block text-xs text-text"}>APIs e Backend</span>
-            <span className={"text-sm font-semibold text-muted-foreground"}>
-              Integrações e consumo de APIs, com Node.js, Express e bancos de
-              dados.
-            </span>
-          </Card>
-          <Card icon={FaTools}>
-            <span className={"block text-xs text-text"}>Versionamento</span>
-            <span className={"text-sm font-semibold text-muted-foreground"}>
-              Controle de versão com Git, colaboração e organização com Git e
-              GitHub.
-            </span>
-          </Card>
-          <Card icon={FaFigma}>
-            <span className={"block text-xs text-text"}>
-              Design e Prototipação
-            </span>
-            <span className={"text-sm font-semibold text-muted-foreground"}>
-              Criação de interfaces e protótipos com Figma, explorando design de
-              interação e experiência do usuário.
-            </span>
-          </Card> */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+            {cards.map((card, index) => (
+              <Card key={index} icon={card.icon}>
+                <span className={"block text-xs text-text"}>{card.text}</span>
+                <span className={"text-sm font-semibold text-muted-foreground"}>
+                  {card.description}
+                </span>
+              </Card>
+            ))}
+          </div>
         </div>
 
         <div className="lg:col-span-5">
